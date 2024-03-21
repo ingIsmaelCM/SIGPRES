b5b64b7efbeccae71efc39f51a951f8e96f50389
AtrionTechSD <dev@atriontechsd.com>
2024-03-20 22:18:29 -0400

    Removed .idea and logs from git

--- .idea/fileTemplates/Repository.ts
+++
@@ -1,9 +0,0 @@
-import { BaseRepository } from "@/app/repositories/BaseRepository";
-import { ${MODEL_NAME} } from "@source/models";
-
-export default class ${MODEL_NAME}Repository extends BaseRepository<${MODEL_NAME}> {
-  constructor() {
-    super(${MODEL_NAME});
-  }
-
-}
